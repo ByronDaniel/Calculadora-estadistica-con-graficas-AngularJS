ed75631256e031aac3cd879a20b72ca116ae80fd
marlonjt <marlonj3@outlook.es>
2020-07-31 13:51:21 -0500

    sumatoria de valores de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'estadistica';
   valores = [];
-
-  agregarValores(valor) {
-    this.valores.push(valor.value);
+  sumatoria= 0;
+  agregarValores(valor){
+    this.valores.push(parseInt(valor.value));
     if( valor ) {
       valor.value = "";
     }
     this.valores.sort((a, b) => a - b);
-  }
+    this.sumatoria=0;
+    this.valores.forEach(element => {
+      this.sumatoria += element
+    });
+    console.log(this.sumatoria);
+ }
 
 }
